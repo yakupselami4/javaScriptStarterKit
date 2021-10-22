371aed60dddf1fb78b216f354df7b1de143e2f6d
yakupselami4 <yakupselami4@gmail.com>
2021-10-23 00:36:33 +0300

    JavaScript Kamp İlk Ders

---
+++ src/app.js
@@ -0,0 +1,35 @@
+console.log("Merhaba kodlama.io")
+
+let dolarDun = 9.20
+let dolarBugun = 9.30
+
+{
+    let dolarDun = 9.10
+}
+
+console.log(dolarDun)
+
+//let ile kullandığımız için 
+//console yazdırırken parantez 
+//dışındaki değeri gösterdi eğer 
+//var ile kullanmış olsaydım parantez 
+//içindeki yani son verilen değeri ekrana döndürecekti.
+//JS typesafe değildir!!!!!
+
+const euroDun=11
+//const sabit bir değerdir daha sonra değer veremeyiz javadaki final gibi
+
+console.log(euroDun)
+//camelCasing*
+//PascalCasing
+
+let konutKredileri = ["Konut Kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel Konut Kredisi"]
+//arrayimizin içine istediğimiz tipte değişken tanımlayabiliriz
+for (let i=0; i<konutKredileri.length; i++) {
+    console.log(konutKredileri[i])
+}
+//console.log(konutKredileri)
+
+
+
+
